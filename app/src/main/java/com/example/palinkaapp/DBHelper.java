64a3c0813ede1d3029e88d7447728e733ff22b3c
Szabolcs Bánhidi <banhidi.szabolcs@petrik.hu>
2020-12-05 23:36:36 +0100

    Adatbázis létrehozása. INSERT, SELECT tesztelése.

---
+++ app/src/main/java/com/example/palinkaapp/DBHelper.java
@@ -0,0 +1,67 @@
+package com.example.palinkaapp;
+
+import android.content.ContentValues;
+import android.content.Context;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+public class DBHelper extends SQLiteOpenHelper
+{
+    public static final int DB_VERSION = 1;
+    public static final String DB_NAME = "palinkaAdatbazis.db";
+    public static final String PALINKA_TABLE = "palinka";
+
+    public static final String COL_ID = "id";
+    public static final String COL_FOZO = "fozo";
+    public static final String COL_GYUMOLCS = "gyumolcs";
+    public static final String COL_ALKOHOL = "alkohol";
+
+    public DBHelper(Context context)
+    {
+        super(context, DB_NAME, null, DB_VERSION);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db)
+    {
+        String sql = "CREATE TABLE " + PALINKA_TABLE + "(" +
+                COL_ID + " INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL, " +
+                COL_FOZO + " VARCHAR(255) NOT NULL, " +
+                COL_GYUMOLCS + " VARCHAR(255) NOT NULL, " +
+                COL_ALKOHOL + " INTEGER NOT NULL, " +
+                "UNIQUE (" + COL_FOZO + "," + COL_GYUMOLCS + ")" +
+                ")";
+        db.execSQL(sql);
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion)
+    {
+        String sql = "DROP TABLE IF EXISTS " + PALINKA_TABLE;
+        db.execSQL(sql);
+        onCreate(db);
+    }
+
+
+    //SELECT
+    public Cursor adatLekerdezes()
+    {
+        //String nev ="Jozsi";
+        SQLiteDatabase db = this.getReadableDatabase();
+        //return db.rawQuery("SELECT * FROM "+TANULO_TABLE+" WHERE nev = ?",new String[]{nev});
+        return db.query(PALINKA_TABLE,new String[]{COL_ID,COL_FOZO,COL_GYUMOLCS,COL_ALKOHOL},null,null,null,null,null,null);
+    }
+
+    //INSERT
+    public boolean adatRogzites(String fozo, String gyumolcs, String alkohol)
+    {
+        SQLiteDatabase db = this.getWritableDatabase();
+        ContentValues values = new ContentValues();
+        values.put(COL_FOZO, fozo);
+        values.put(COL_GYUMOLCS, gyumolcs);
+        values.put(COL_ALKOHOL, alkohol);
+        long result = db.insert(PALINKA_TABLE, null, values);
+        return result != -1;
+    }
+}
--- app/src/main/java/com/example/palinkaapp/MainActivity.java
+++ app/src/main/java/com/example/palinkaapp/MainActivity.java
@@ -2,13 +2,56 @@ package com.example.palinkaapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.database.Cursor;
 import android.os.Bundle;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    DBHelper adatbazis;
+    TextView textAdatok;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        init();
+        adatbazis.adatRogzites("Pécsi pálinka","Körte","30");
+        adatLekerdezes();
+
+    }
+
+    public void init()
+    {
+        adatbazis = new DBHelper(MainActivity.this);
+        textAdatok = findViewById(R.id.textAdatok);
+
+    }
+
+    private void adatLekerdezes()
+    {
+        Cursor adatok = adatbazis.adatLekerdezes();
+        if (adatok == null)
+        {
+            Toast.makeText(this, "Sikertelen lekérdezés", Toast.LENGTH_SHORT).show();
+            return;
+        }
+        if (adatok.getCount()==0)
+        {
+            Toast.makeText(this, "Üres az adatbázis", Toast.LENGTH_SHORT).show();
+            return;
+        }
+        StringBuilder stringBuffer = new StringBuilder();
+        while (adatok.moveToNext())
+        {
+            stringBuffer.append("ID: "+adatok.getString(0)+"\n");
+            stringBuffer.append("Főző: "+adatok.getString(1)+"\n");
+            stringBuffer.append("Gyümölcs: "+adatok.getString(2)+"\n");
+            stringBuffer.append("Alkohol: "+adatok.getString(3)+"\n");
+        }
+        textAdatok.setText(stringBuffer.toString());
+        Toast.makeText(this, "sikeres lekérdezés", Toast.LENGTH_SHORT).show();
     }
 }
